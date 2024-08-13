af98242a011b8f1dda9fae481d3ecacd20529dc7
kunal-2668 <kunal0.kr@gmail.com>
2024-08-13 11:57:37 +0530

    first commit

--- Pinterest_Clone/settings.py
+++ Pinterest_Clone/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-y7!7k@+lprvd5%g0r%1d)g9#up0-#%i!e6e-7e9x&z9^)ve!45
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
